32f9109467edef77eb0c86a6146bb4c8a3f0c5a3
root <gogolgrind@gmail.com>
2020-04-27 22:27:58 +0000

    update run.sh with some comments

--- Week 2/Day 3/Submissions/ksozykin/run.sh
+++ Week 2/Day 3/Submissions/ksozykin/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 data_dir='./nycflights/' 
 temp_dir='./tmp/' # to avoid temp files in /tmp due to remote machine specification
-n_jobs=8
+n_jobs=8 # also trained n_jobs 4, found that  at least on this remote machine args.threads_per_worker more important
 mkdir -p $data_dir
-python3 download.py -d $data_dir -n 1000
+python3 download.py -d $data_dir -n 1000 # also traied n_rows 10000, in both cases all csv files is used
 python3 main.py -d $data_dir -t $temp_dir -j $n_jobs
